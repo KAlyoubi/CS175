5e818b23ce5ca538cfb99cedee538b8b44101fa3
s1292637 <s1292637@cssemaclin17.monmouth.edu>
2019-09-16 21:00:24 -0400

    Variables Created

---
+++ HousePainting/src/PaintCost.java
@@ -0,0 +1,34 @@
+
+public class PaintCost {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * This program will calculate the cost to paint a house
+		 */
+		int HouseLengthFeet=20;
+		int inches=6;
+		System.out.println(HouseLengthFeet + inches/12);
+		double houseLengthFeet;
+		double houseLengthInch;
+		double houseWedthFeet;
+		double houseWedthInch;
+		double houseHeightFeet;
+		double houseHeightINCH;
+		double windowLenghtFeet;
+		double windowLenghtInch;
+		double windowwidthFeet;
+		double windowwidthInch;
+		int windowNumber;
+		double doorLenghtFeet;
+		double doorLenghtInch;
+		double doorWidthFeet;
+		double doorWidthInch;
+		int doorNumber;
+		double sqFtCost;
+		double sqFtNormal;
+		double sqFtWindows;
+		double sqFtDoors;
+	}
+
+}
