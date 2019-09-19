cf55eaa54c3f39bc28391260d990d05188a85ce0
s1292637 <s1292637@cssemaclin17.monmouth.edu>
2019-09-18 20:58:25 -0400

    Work in progress

--- HousePainting/src/PaintCost.java
+++ HousePainting/src/PaintCost.java
@@ -1,34 +1,101 @@
+import java.util.Scanner;
 
 public class PaintCost {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		/*
-		 * This program will calculate the cost to paint a house
-		 */
-		int HouseLengthFeet=20;
-		int inches=6;
-		System.out.println(HouseLengthFeet + inches/12);
-		double houseLengthFeet;
-		double houseLengthInch;
-		double houseWedthFeet;
-		double houseWedthInch;
-		double houseHeightFeet;
-		double houseHeightINCH;
-		double windowLenghtFeet;
-		double windowLenghtInch;
-		double windowwidthFeet;
-		double windowwidthInch;
-		int windowNumber;
-		double doorLenghtFeet;
-		double doorLenghtInch;
-		double doorWidthFeet;
+
+		//double houseLengthFeet;
+		
+		//double houseLengthInch;
+		
+		//double houseWidthFeet;
+		
+		//double houseWidthInch;
+		
+		//double houseHeightFeet;
+		
+		//double houseHeightInch;
+		
+		//double windowLenghtFeet;
+		
+		//double windowLenghtInch;
+		
+		//double windowWidthFeet;
+		
+		//double windowWidthInch;
+		
+		//int windowNumber;
+		
+		//double doorLenghtFeet;
+		
+		//double doorLenghtInch;
+		
+		//double doorWidthFeet;
+		
 		double doorWidthInch;
+		
 		int doorNumber;
+		
 		double sqFtCost;
+		
 		double sqFtNormal;
+		
 		double sqFtWindows;
+		
 		double sqFtDoors;
+		
+		Scanner in = new Scanner(System.in);
+				
+				System.out.println("Enter Length of the house in feet ");
+				double houseLengthFeet = in.nextDouble();
+				
+				System.out.println("Enter Length of the house in inch ");
+				double houseLengthInch = in.nextDouble();
+
+				System.out.println("Enter Width of the house in feet ");
+				double houseWidthFeet = in.nextDouble();
+				
+				System.out.println("Enter Width of the house in inch ");
+				double houseWidthInch = in.nextDouble();
+
+				System.out.println("Enter Height of the house in feet ");
+				double houseHeightFeet = in.nextDouble();
+				
+				System.out.println("Enter Height of the house in inch ");
+				double houseHeightInch = in.nextDouble();
+				
+				System.out.println("Enter Length of the window in feet ");
+				double windowLenghtFeet = in.nextDouble();
+				
+				System.out.println("Enter Length of the window in inch ");
+				double windowLenghtInch = in.nextDouble();
+				
+				System.out.println("Enter Width of the window in feet ");
+				double windowWidthFeet = in.nextDouble();
+				
+				System.out.println("Enter Width of the window in inch ");
+				double windowWidthInch = in.nextDouble();
+				
+				System.out.println("Enter numbr of windows ");
+				int windowNumber = in.nextInt();
+				
+				System.out.println("Enter Length of the door in feet ");
+				double doorLenghtFeet = in.nextDouble();
+				
+				System.out.println("Enter Length of the door in inch ");
+				double doorLenghtInch = in.nextDouble();
+				
+				System.out.println("Enter Width of the door in feet ");
+				double doorWidthFeet = in.nextDouble();
+				
+				System.out.println("Enter Width of the door in inch ");
+				double doorWidthInch = in.nextDouble();
+
+
+
+
 	}
+	
 
 }
