00f210eb2bf6c9c5df244526be3152bd9cc3cf8c
KAlyoubi <55116510+KAlyoubi@users.noreply.github.com>
2019-11-06 18:18:34 -0500

    Add files via upload

---
+++ E52.java
@@ -0,0 +1,31 @@
+package ch5;
+
+import java.util.Scanner;
+
+public class E52 {
+
+
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+ 
+ 
+		System.out.println("Enter a number: ");
+		float num = in.nextFloat();
+ 
+		if(num < 0 ) {
+  
+			if(Math.abs(num) < 0) {
+  }
+  
+			else if (Math.abs(num) > 1000000) {
+  }
+			
+ }
+		
+		System.out.print("The number  is negative and small");
+		System.out.println(" and Large");
+
+}
+
+}
